f551edff11265fcdcac28c0342efa7b376ee17f4
unknown <binobajichiku@gmail.com>
2023-09-18 23:42:58 +0530

    added front end

--- client/src/Page/Signup.jsx
+++ client/src/Page/Signup.jsx
@@ -33,24 +33,22 @@ function Signup() {
   else{
     let res=await axios.post("http://localhost:3035/users/register", formData).
     then((res)=>{console.log(res.message);
-     
     return res.message;
     })
       .catch((err) =>{console.log(err)
       return err;}
       );
     }
-
 }
 
   return (
     <div width="30%" margin="auto">
       <input placeholder="Enter Phone No" onChange={handleChange} name="phoneno" />
       <input placeholder="Enter Ipaddress" onChange={handleChange} name="ipaddress" />
+      <>{formData.showotp?<input placeholder="Enter Otp" onChange={handleChange} name="otp" />:""}</>
       <button variant="contained" onClick={handleSubmit}>
-        Register
+       {formData.showotp?" Register":"Generate Otp"}
       </button >
-      {}
     </div>
   );
 }
