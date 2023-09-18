23bb5f1a0585e3bea5ee25c6119c53e5d8d5e4e0
unknown <binobajichiku@gmail.com>
2023-09-18 23:20:14 +0530

    added signup

--- client/src/Page/Signup.jsx
+++ client/src/Page/Signup.jsx
@@ -4,9 +4,10 @@ import { useState } from "react";
 import axios from "axios";
 
 const initialState = {
-  name:"" , email: "",
-  password: "", gender:"",
+  phoneno:"",
   ipaddress:"",
+  otp:"",
+  showotp:false,
 };
 
 function Signup() {
@@ -16,24 +17,32 @@ function Signup() {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
-    axios.post("http://localhost:3035/users/register", formData).then((res) => console.log(res.data)).catch((err) => console.log(err));
-  };
+  const handleSubmit = async(e) => {
+
+    if(!formData.showotp){
+  let res=await axios.post("http://localhost:3035/users/register", formData).
+  then((res)=>{console.log(res.message);
+    setFormData((prev)=>({...prev,showotp:true}));
+  return res.message;
+  })
+    .catch((err) =>{console.log(err)
+    return err;}
+    );
+  }
+  
+  else{
+    console.log("sfg");
+  }
+}
 
   return (
     <div width="30%" margin="auto">
-      <input placeholder="Enter Email" onChange={handleChange} name="email" />
-      <input
-        placeholder="Enter Password"
-        onChange={handleChange}
-        name="password"
-      />
-      <input placeholder="Enter Gender" onChange={handleChange} name="gender" />
-      <input placeholder="Name" onChange={handleChange} name="name" />
-
+      <input placeholder="Enter Phone No" onChange={handleChange} name="phoneno" />
+      <input placeholder="Enter Ipaddress" onChange={handleChange} name="ipaddress" />
       <button variant="contained" onClick={handleSubmit}>
         Register
       </button >
+      {}
     </div>
   );
 }
