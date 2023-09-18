da0c6d74de9449a45369d0048ef99249148bf4fd
unknown <binobajichiku@gmail.com>
2023-09-19 00:07:28 +0530

    added teilio

--- server/index.js
+++ server/index.js
@@ -36,3 +36,30 @@ connectDatabase().then(() => {
     );
   });
 });
+
+
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Set environment variables for your credentials
+// Read more at http://twil.io/secure
+const accountSid = "ACfb18525c5c98a8ea54f8da57b170b4a0";
+const authToken = "234352412ad88b6ee412d9c06fd3fb69";
+const verifySid = "VA2898d7098064df8634fe5294e3b8164c";
+const client = require("twilio")(accountSid, authToken);
+
+client.verify.v2
+  .services(verifySid)
+  .verifications.create({ to: "+916370457871", channel: "sms" })
+  .then((verification) => console.log(verification.status))
+  .then(() => {
+    const readline = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    readline.question("Please enter the OTP:", (otpCode) => {
+      client.verify.v2
+        .services(verifySid)
+        .verificationChecks.create({ to: "+916370457871", code: otpCode })
+        .then((verification_check) => console.log(verification_check.status))
+        .then(() => readline.close());
+    });
+  });
