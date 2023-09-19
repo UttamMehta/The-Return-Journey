8301b7e091641634114fe46e18e6647d73c858d2
unknown <binobajichiku@gmail.com>
2023-09-19 23:03:50 +0530

    added message

--- server/controllers/try.js
+++ server/controllers/try.js
@@ -3,25 +3,51 @@ const authToken = "234352412ad88b6ee412d9c06fd3fb69";
 const verifySid = "VA2898d7098064df8634fe5294e3b8164c";
 const client = require("twilio")(accountSid, authToken);
 
-function generateOTP() {
-    // Generate a 6-digit OTP (you can adjust the length as needed)
-    return Math.floor(100000 + Math.random() * 900000).toString();
-  }
 
-client.verify.v2
-  .services(verifySid)
-  .verifications.create({ to: "+919437939830", channel: "sms" })
-  .then((verification) => console.log(verification.status))
-  .then(() => {
-    const readline = require("readline").createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-    readline.question("Please enter the OTP:", (otpCode) => {
-      client.verify.v2
-        .services(verifySid)
-        .verificationChecks.create({ to: "+919437939830", code: otpCode })
-        .then((verification_check) => console.log(verification_check.status))
-        .then(() => readline.close());
-    });
-  });
+
+// client.verify.v2
+//   .services(verifySid)
+//   .verifications.create({ to: "+919437939830", channel: "sms" })
+//   .then((verification) => console.log(verification.status))
+//   .then(() => {
+//     const readline = require("readline").createInterface({
+//       input: process.stdin,
+//       output: process.stdout,
+//     });
+//     readline.question("Please enter the OTP:", (otpCode) => {
+//       client.verify.v2
+//         .services(verifySid)
+//         .verificationChecks.create({ to: "+919437939830", code: otpCode })
+//         .then((verification_check) => console.log(verification_check.status))
+//         .then(() => readline.close());
+//     });
+//   });
+
+
+  function sendRandomOTP(phoneNumber) {
+    client.verify
+      .v2.services(verifySid)
+      .verifications.create({ to: phoneNumber, channel: 'sms', })
+      .then((verification) => {
+        console.log(verification.sid);
+        // You can also store the OTP in your database or session for verification later
+      })
+      .catch((error) => {
+        console.error('Error sending OTP:', error);
+      });
+  }
+// client.verify.v2
+//   .services(verifySid)
+//   .verifications.create({
+//     channel: 'sms',
+//     to:"+918338853833",
+//    })
+//   .then(message => console.log(message.sid))
+//   .catch((error)=>{
+// console.log("error on 52");
+//   });
+  
+  // Example usage: Send a random OTP to the user's phone number
+  const userPhoneNumber = '+919437939830'; // Replace with the user's phone number
+  sendRandomOTP(userPhoneNumber);
+  
