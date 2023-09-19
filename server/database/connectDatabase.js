5553c7d3524085c9c12380f1224520e224225f3d
unknown <binobajichiku@gmail.com>
2023-09-19 19:12:47 +0530

    added url

--- server/database/connectDatabase.js
+++ server/database/connectDatabase.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 const config = require("../config/config");
+// const config = require("./config");
 mongoose.set("strictQuery", false);
 
 async function connectDatabase() {
-  const result = await mongoose.connect("mongodb://localhost:27017/userdata");
-
-  return result;
+  try {
+    // console.log(config);
+    const result = await mongoose.connect(config.DB_CONNECTION_URL);
+    return result;
+  } catch (e) {
+    return e;
+  }
 }
 
 module.exports = connectDatabase;
