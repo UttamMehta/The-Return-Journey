e8d5a08e9c6a26eb2155137fa68133baccb3a2be
unknown <binobajichiku@gmail.com>
2023-09-20 08:37:26 +0530

    added auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,6 +3,10 @@ const jwt = require("jsonwebtoken");
 const config = require("../config/config");
 const axios = require("axios");
 const brcypt = require("bcryptjs");
+const accountSid = "ACfb18525c5c98a8ea54f8da57b170b4a0";
+const authToken = "234352412ad88b6ee412d9c06fd3fb69";
+const verifySid = "VA2898d7098064df8634fe5294e3b8164c";
+const client = require("twilio")(accountSid, authToken);
 
 function generateToken(user) {
   const { _id,phoneno,ipaddress} = user;
@@ -117,11 +121,37 @@ async function login(req, res) {
 }
 
 
-
+async function sendRandomOTP(req,res) {
+  
+  try {
+    let {phoneno}=req.body;
+    phoneno="+91"+phoneno;
+    
+    client.verify
+    .v2.services(verifySid)
+    .verifications.create({ to: phoneno, channel: 'sms', })
+    .then((verification) => {
+      console.log(verification);
+      return res.send({
+        message: "Otp send successful",
+      });
+      // we can also store the OTP in your database or session for verification later
+    })
+    .catch((error) => {
+      console.log('Error sending OTP:', error);
+    });
+    
+  } catch (error) {
+    console.log("Error in 26 on otp");
+    return res.status(400).send({error:"Not able to send otp try later"});
+  }
+  
+}
 
 
 module.exports = {
   register,
   login,
   getAllUser,
+  sendRandomOTP
 };
